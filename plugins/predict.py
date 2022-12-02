56990d203b16e822f32397e368c140b96afd0875
Gomicoder17 <202113370@alu.comillas.edu>
2022-12-02 20:58:21 +0100

    Fixed bug

--- plugins/predict.py
+++ plugins/predict.py
@@ -29,6 +29,10 @@ def get_days():
     monday = pd.Timestamp(year=2015, month=today.month, day=today.day)
     while monday.dayofweek != 0:
         monday += pd.Timedelta(days=1)
+    if monday.year == 2016:
+        monday = pd.Timestamp(year=2017, month=today.month, day=today.day)
+        while monday.dayofweek != 0:
+            monday -= pd.Timedelta(days=1)
     return today, monday
 
 
